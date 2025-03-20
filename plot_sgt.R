56838de8f8489c034e2b0d50eff6d999420ae9e4
Sheng Long <shenglong@u.northwestern.edu>
2025-03-20 09:37:29 -0500

    adding things

---
+++ plot_sgt.R
@@ -0,0 +1,16 @@
+library(sgt)
+library(ggplot2)
+
+x_vals <- seq(-5, 5, length.out = 1000)
+pdf_vals <- dsgt(x_vals, mu=0.3, sigma=1, lambda=0.6, p=3, q=10, 
+                 mean.cent = FALSE)
+med <- qsgt(0.5, mu=0.3, sigma=1, lambda=0.6, p=3, q=10, 
+            mean.cent = FALSE)
+
+data.frame(x = x_vals, density = pdf_vals) %>% 
+  ggplot(aes(x = x, y = density)) + 
+  geom_line() + 
+  theme_minimal() + 
+  ylim(0, 1) + 
+  geom_vline(xintercept = 0.3, linetype = "dashed", color = "red") + 
+  geom_vline(xintercept = med, linetype = "dashed", color = "blue")
