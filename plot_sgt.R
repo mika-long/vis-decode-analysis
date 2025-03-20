9d89442334687fd8b8cace1b861b21267431e91c
Sheng Long <shenglong@u.northwestern.edu>
2025-03-20 13:12:36 -0500

    all the changes made before the meeting

--- plot_sgt.R
+++ plot_sgt.R
@@ -2,9 +2,9 @@ library(sgt)
 library(ggplot2)
 
 x_vals <- seq(-5, 5, length.out = 1000)
-pdf_vals <- dsgt(x_vals, mu=0.3, sigma=1, lambda=0.6, p=3, q=10, 
+pdf_vals <- dsgt(x_vals, mu=0.3, sigma=1, lambda=0.8, p=3, q=10, 
                  mean.cent = FALSE)
-med <- qsgt(0.5, mu=0.3, sigma=1, lambda=0.6, p=3, q=10, 
+med <- qsgt(0.5, mu=0.3, sigma=1, lambda=0.8, p=3, q=10, 
             mean.cent = FALSE)
 
 data.frame(x = x_vals, density = pdf_vals) %>% 
@@ -13,4 +13,5 @@ data.frame(x = x_vals, density = pdf_vals) %>%
   theme_minimal() + 
   ylim(0, 1) + 
   geom_vline(xintercept = 0.3, linetype = "dashed", color = "red") + 
-  geom_vline(xintercept = med, linetype = "dashed", color = "blue")
+  geom_vline(xintercept = med, linetype = "dashed", color = "blue") + 
+  labs(title = "lambda = 0.8")
