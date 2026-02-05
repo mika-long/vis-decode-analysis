80d95e137e735ba50e490d8d86b8a32130305372
Sheng Long <shenglong@u.northwestern.edu>
2026-02-05 12:21:46 -0600

    generated configuration! works

--- R/gen_config.R
+++ R/gen_config.R
@@ -1,4 +1,18 @@
 library(jsonlite)
+library(purrr)
+library(here)
+
+# --- Get the list of numbers to pull from
+exp_json_data <- jsonlite::fromJSON(here("data", "moritz", "stimuli", "exp2-stimuli.json" )) %>% as_tibble(.)
+
+stimuli_df <- exp_json_data %>% select(index, seed, noise, flip, type, data) %>% 
+    mutate(noise = as.character(noise))
+
+point_ids <- stimuli_df %>% filter(type == "point") %>% 
+  pull(index)
+
+pointArc_ids <- stimuli_df %>% filter(type == "point_arc") %>% 
+  pull(index)
 
 #--- Define UI and metada 
 
@@ -48,36 +62,96 @@ BaseComponent <- function(type, path, instruction, instructionLocation, nextButt
   )
 }
 
-my_responses <- list(
-  StudyResponse(
-    id = "pixelsPerMM", 
-    prompt = "Calibration results", 
-    type = "reactive", # Note: 'reactive' is a specific string here, not an R concept
-    required = TRUE, 
-    hidden = TRUE
+Component <- function(baseComponent, parameters){
+  list(
+    baseComponent = baseComponent,
+    parameters = parameters
+  )
+}
+
+convert_to_json <- function(c){
+  toJSON(c, auto_unbox = TRUE, pretty = TRUE)
+}
+
+
+# --- construct response 
+moritz_response = StudyResponse(
+  id="weightedAverage-value",
+  prompt="The value of response",
+  required = FALSE,
+  type = "numerical",
+  hidden = TRUE
+)
+
+# --- construct basecomponent 
+moritz_component <- BaseComponent(
+  type="react-component",
+  path="vis-decode-retrieve-value/assets/Moritz.tsx",
+  instruction= "Drag the slider to where the values are on average; imagine that this is some stock prices",
+  instructionLocation = "aboveStimulus",
+  nextButtonLocation = "belowStimulus",
+  parameters = list(
+    taskid = "Mortiz",
+    taskType = "mortiz"
+  ),
+  response_list = list(moritz_response)
+)
+# test 
+convert_to_json(moritz_component)
+
+# creating a simple component 
+task1 = Component(baseComponent = "Moritz", parameters = list(params = list(index = 4)))
+convert_to_json(task1)
+
+# creating a sequence of components 
+components <- point_ids %>% imap(~ Component(
+  baseComponent = "Mortiz", 
+  parameters = list(params = list(index = .x))
+)) %>% 
+  set_names(paste0("task", seq_along(point_ids)))
+
+# build the sequence 
+task_names = paste0("task", 1:48)
+sequence <- list(
+  order = "fixed", 
+  components = list(
+    "introduction", 
+    "$virtual-chinrest.se.full",
+    list(
+      order = "random",
+      components = c(
+        task_names
+      )
+    )
   )
 )
-
-# C. Create the Component
-card_component <- BaseComponent(
-  type = "react-component",
-  path = "vis-decode-click/assets/VirtualChinrestCalibration.tsx",
-  nextButtonLocation = "belowStimulus",
-  parameters = list(taskid = "pixelsPerMM"),
-  response_list = my_responses
-)
+sequence %>% convert_to_json(.)
 
 # 4. RENDER
-# We convert to JSON only at the very end.
-# auto_unbox = TRUE handles the single strings.
+# the final output has the following components: 
+# 1. schema 
+# 2. studyMetadata 
+# 3. uiConfig, 
+# 4. importedLibraries 
+# 5. baseComponents 
+# 6. components 
+# 7. sequence 
 json_output <- toJSON(card_component, auto_unbox = TRUE, pretty = TRUE)
 
 
 final_output = list(
+  schema = "https://raw.githubusercontent.com/revisit-studies/study/v2.3.1/src/parser/StudyConfigSchema.json",
   studyMetadata = metadata, 
   uiConfig = ui_config,
-  importedLibraries = list()
+  importedLibraries = list("virtual-chinrest"), 
+  BaseComponens = list(
+    Moritz = moritz_component
+  ), 
+  components = components,
+  sequence=sequence
 )
 
-toJSON(final_output, auto_unbox = TRUE, pretty = TRUE)
+# save output 
+final_output %>% convert_to_json(.) %>%
+  write(here("R", "config.json"))
 
