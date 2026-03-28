7fdcf2641dc2aad97a45b01f2664134e6c4217a4
Sheng Long <shenglong@u.northwestern.edu>
2026-03-28 13:38:43 -0500

    generated all inset graphs with answer annotated

--- R/gen_graph.R
+++ R/gen_graph.R
@@ -6,6 +6,51 @@ library(ggplot2)
 library(tidyverse)
 
 ##########
+# Plot a new example as inset graphs -- Highest Point -- CDF Mode
+##########
+x <- seq(-5, 5, length.out = 100)
+y <- dsgt(
+  x,
+  mu = -1.1,
+  sigma = 0.9,
+  lambda = -0.3,
+  p = 2.6,
+  q = 2.17,
+  mean.cent = FALSE
+)
+x_val <- -1.1
+y_val <- dsgt(
+  x_val,
+  mu = -1.1,
+  sigma = 0.9,
+  lambda = -0.3,
+  p = 2.6,
+  q = 2.17,
+  mean.cent = FALSE
+)
+example_plot <- tibble(x = x, y = y) %>%
+  ggplot(aes(x = x, y = y)) +
+  geom_line() +
+  annotate("point", x = x_val, y = y_val, color = "red", size = 2) +
+  coord_cartesian(xlim = c(-5, 5), ylim = c(0, 1)) +
+  theme(
+    axis.title.x = element_blank(), # Removes only the x-axis title
+    axis.title.y = element_blank() # Removes only the y-axis title
+  ) +
+  tidybayes::theme_tidybayes()
+
+example_plot
+
+ggsave(
+  here::here("figs", "HighestPoint_exmaple.svg"),
+  example_plot,
+  width = 3,
+  height = 2.25,
+  unit = "in",
+  dpi = 300
+)
+
+##########
 # Plot a new example as inset graphs -- Max Slope -- CDF Mode
 ##########
 
